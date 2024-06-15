blackeangel/e2fstool

commit f2a1a2480a3a14210b22540501672b914989c358

feat: add option to only extract config files * now, original timestamps are respected in the extracted files. * final UB fixes (when using / as mountpoint) * restored "asprintf" for better string allocations handling. * addressed a bunch of memory leaks
Signed-off-by: svoboda18 <dev.svoboda18@gmail.com>

e2fstool.h
@@ -14,9 +14,9 @@
 #include <private/android_filesystem_capability.h>
 
 #define E2FSTOOL_VERSION "1.0.0"
-#define E2FSTOOL_DATE "31-May-2024"
+#define E2FSTOOL_DATE "16-July-2024"
 
-#define E2FSTOOL_ERROR(pfx, ...) printf("%s: %s" pfx "\n", __func__, strerror(errno) __VA_OPT__(,) __VA_ARGS__)
+#define E2FSTOOL_ERROR(pfx, ...) printf("%s: %s " pfx "\n", __func__, strerror(errno) __VA_OPT__(,) __VA_ARGS__)
 
 #ifdef SVB_MINGW
 #ifdef HAVE_LIB_NT_H
@@ -34,12 +34,11 @@
 #endif
 
 #define FILE_MODE_MASK 0x0FFF
-#define FILE_READ_BUFLEN (1 << 26)
+#define FILE_READ_BUFLEN (1 << 27)
 #define RESERVED_INODES_COUNT 0xA /* Excluding EXT2_ROOT_INO */
 #define SYMLINK_I_BLOCK_MAX_SIZE 0x3D
 
 #define SPARSE_HEADER_MAGIC 0xed26ff3a
-#define EXT4_SUPERBLOCK_MAGIC 0xef53
 
 typedef enum image_type {
     SPARSE,
@@ -50,6 +49,5 @@ typedef enum image_type {
 struct inode_params {
     char *path;
     char *filename;
-    size_t len;
 };
 #endif /* E2FSTOOL_H_INC */

commit c1c4cc2288c46579c8188160cd2cbc27437b53ad

fix: ino_extract_regular: do not decrement got * this a regression introduced in commit f2a1a24, which   caused the ino_extract_regular() function to decrement   the 'got' variable, thus makes the loop exits and leaves   unextracted data for files size > 1 << 27. * correct the build date.
Signed-off-by: svoboda18 <dev.svoboda18@gmail.com>

e2fstool.c
@@ -378,7 +378,6 @@ errcode_t ino_extract_regular(ext2_filsys fs, ext2_ino_t ino, const char *path)
             goto close;
         }
 
-        got -= nbytes;
         written += nbytes;
     } while (got);
 
@@ -578,6 +577,7 @@ static errcode_t walk_fs(ext2_filsys fs)
             return retval;
         }
     }
+
     if (android_configure)
     {
         if (mountpoint)
@@ -632,6 +632,17 @@ static errcode_t walk_fs(ext2_filsys fs)
     {
         goto end;
     }
+
+#ifdef SVB_MINGW
+    if (!android_configure_only)
+    {
+        retval = set_path_timestamp(out_dir, inode.i_atime, inode.i_mtime, inode.i_ctime);
+        if (retval)
+        {
+            E2FSTOOL_ERROR("while configuring timestamps for %s", out_dir);
+        }
+    }
+#endif
 
     if (!quiet && !verbose)
         ext2fs_numeric_progress_close(fs, &progress, "done\n");

e2fstool.h
@@ -14,7 +14,7 @@
 #include <private/android_filesystem_capability.h>
 
 #define E2FSTOOL_VERSION "1.0.0"
-#define E2FSTOOL_DATE "16-July-2024"
+#define E2FSTOOL_DATE "15-July-2024"
 
 #define E2FSTOOL_ERROR(pfx, ...) printf("%s: %s " pfx "\n", __func__, strerror(errno) __VA_OPT__(,) __VA_ARGS__)
 